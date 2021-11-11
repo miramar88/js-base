3b3752ba05858085b97178155a81701744b2dd1c
Margarita Smirnova <bobcat0606@gmail.com>
2021-11-11 13:56:05 +0300

    add task 2

--- script2.js
+++ script2.js
@@ -1,16 +1,12 @@
-// let x = 'somewords';
-
-// if (typeof (x) == "string") {
-//   console.log('x - строка');
-// } else if (typeof (x) == "number") {
-//   console.log('x - число');
-// } else if (typeof (x) == "boolean") {
-//   console.log('x - логический тип');
-// } else {
-//   console.log('x относится к другому типу данных');
-// }
-
-let y = Math.random();
+let x;
+if (typeof (x) == "string") {
+  console.log('x - строка');
+} else if (typeof (x) == "number") {
+  console.log('x - число');
+} else if (typeof (x) == "boolean") {
+  console.log('x - логический тип');
+} else {
+  console.log('x относится к другому типу данных');
+}
 
 
-console.log(Math.round(y * 10));
