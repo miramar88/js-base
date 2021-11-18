1e5dc39ac6fe6e72dab596ce0dc4a03e577c11e4
Margarita Smirnova <bobcat0606@gmail.com>
2021-11-18 11:19:55 +0300

    add task 9

---
+++ script9.js
@@ -0,0 +1,27 @@
+let arr = [-10, "flowers", 125, "birds", 34, 0, true, 1000, 111, 4, 99, 33, 0, 11, "trees"];
+let even = [];
+let odd = [];
+let nullElem = [];
+
+function arrayNumberType(array) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == 'number') {
+      if (arr[i] % 2 == 0 && arr[i] != 0) {
+        even.push(arr[i]);
+      } else if (arr[i] == 0) {
+        nullElem.push(arr[i]);
+      } else {
+        odd.push(arr[i]);
+      }
+    }
+  }
+
+  console.log('Четных чисел - ' + even.length);
+  console.log('Нечетных чисел - ' + odd.length);
+  console.log('Нулей - ' + nullElem.length);
+}
+
+arrayNumberType(arr);
+
+
+
